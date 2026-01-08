52cc12d68b423559cb5716efda7b32af23a9aefb
THIEN <thanhthien.devp@gmail.com>
2026-01-09 00:40:51 +0700

    fix: IOS error

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import WebKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,25 +7,6 @@ import WebKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Configure WebView for media playback without user gesture
-    if #available(iOS 15.0, *) {
-      let preferences = WKPreferences()
-      preferences.javaScriptEnabled = true
-      
-      let configuration = WKWebViewConfiguration()
-      configuration.preferences = preferences
-      configuration.allowsInlineMediaPlayback = true
-      configuration.mediaTypesRequiringUserActionForPlayback = []
-    } else if #available(iOS 10.0, *) {
-      let preferences = WKPreferences()
-      preferences.javaScriptEnabled = true
-      
-      let configuration = WKWebViewConfiguration()
-      configuration.preferences = preferences
-      configuration.allowsInlineMediaPlayback = true
-      configuration.mediaPlaybackRequiresUserGesture = false
-    }
-    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
